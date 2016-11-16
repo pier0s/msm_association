bbc793ee441c1ce5c1ea96fd4c664c3482b29d93
Raghu Betina <raghu@firstdraft.com>
2016-11-16 01:36:10 +0000

    Validate numericality of movies.duration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,8 @@ class Movie < ApplicationRecord
 
   # Validations
 
+  validates :duration, :numericality => { :less_than => 2764800, :greater_than => 0 }
+
   validates :title, :uniqueness => { :scope => [:year] }
 
   validates :title, :presence => true
